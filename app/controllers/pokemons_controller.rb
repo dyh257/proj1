de38d0633468c191c95d374af609608a86a5ddb7
dyh257@gmail.com <vagrant@rails-dev-box>
2014-11-03 05:08:25 +0000

    I am a Pokemon master.

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,37 @@
 class PokemonsController < ApplicationController
+	def capture
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.trainer_id = current_trainer.id
+		@pokemon.save
+		redirect_to root_path
+	end
+
+	def damage
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.health = @pokemon.health-10
+		@pokemon.save
+		redirect_to trainer_path(current_trainer.id)
+	end
+
+	def heal
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.health = @pokemon.health+10
+		@pokemon.save
+		redirect_to trainer_path(current_trainer.id)
+	end
+
+	def new
+
+	end
+
+	def create
+		@pokemon = Pokemon.new({"name"=>:name, "level"=>1, "health"=>100, "trainer_id"=>current_trainer.id})
+		if @pokemon.save
+			redirect_to trainer_path(current_trainer.id)
+		else
+			redirect_to new_path
+			flash[:error] = @pokemon.errors.full_messages.to_sentence
+		end
+	end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,11 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+  resources :pokemons
+
+  patch 'capture', to: 'pokemons#capture'
+  patch 'damage', to: 'pokemons#damage'
+  patch 'heal', to: 'pokemons#heal'
+  get 'new', to: 'pokemons#new'
+  post 'create', to: 'pokemons#create'
 end
